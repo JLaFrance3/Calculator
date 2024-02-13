c68ce4290e4907f3404689d5a0a6a5407d7fd24c
jlafrance3 <jlafranceiii@proton.me>
2024-02-13 12:21:25 -0500

    Added subtraction functionality

--- Calculator.java
+++ Calculator.java
@@ -4,18 +4,32 @@ public class Calculator {
     
     public static void main(String args[]) {
         
-        int num1;       //user entered number
-        int num2;       //user entered number
+        int num1;           //User entered number
+        int num2;           //User entered number
+        String selection;   //Addition or Subtraction selection
 
         Scanner scan = new Scanner(System.in);
         
+        //Obtain user inputs
         System.out.print("First number:\t");
         num1 = scan.nextInt();
-
         System.out.print("Second number:\t");
         num2 = scan.nextInt();
+        scan.nextLine();
 
-        System.out.printf("%d + %d = %d", num1, num2, num1+num2);
+        //Prompt user for Addition or Subtraction
+        System.out.println("Please enter one of the following:\nAddition\nSubtraction\n");
+        selection = scan.nextLine();
+
+        //Calulate total based on user selection with minor error checking
+        if (selection.toUpperCase().charAt(0) == 'A') {
+            System.out.println("\nYou have selected Addition");
+            System.out.printf("%d + %d = %d", num1, num2, num1+num2);
+        }
+        else {
+            System.out.println("\nYou have selected Subtraction");
+            System.out.printf("%d - %d = %d", num1, num2, num1-num2);
+        }
 
         scan.close();
     }
